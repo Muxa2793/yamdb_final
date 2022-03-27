5c6e331e91e7f2956805a6dc53589321f33a31ca
Mikhail Spiridonov <Muxa2793@mail.ru>
2022-03-27 22:27:11 +0300

    env

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -1,13 +1,12 @@
 import os
 from datetime import date, timedelta
 
+from dotenv import load_dotenv
+
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # dotenv_path = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))) + '/infra/.env'
-# load_dotenv(dotenv_path)
-
-print(os.environ['SECRET_KEY'])
-print(os.environ['DB_ENGINE'])
+load_dotenv()
 
 SECRET_KEY = os.getenv('SECRET_KEY')
 
